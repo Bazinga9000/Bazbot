87826f1cc7d319cf1c4975d5e6f9706d7948a99a
Bazinga <19626297+Bazinga9000@users.noreply.github.com>
2018-08-26 18:55:13 -0400

    Removed mandelbrot

--- stem.py
+++ stem.py
@@ -157,53 +157,6 @@ class Stem():
         self.bot = bot
 
 
-'''
-        self.cmaps = ['Accent', 'Accent_r', 'Blues', 'Blues_r', 'BrBG', 'BrBG_r', 'BuGn', 'BuGn_r', 'BuPu', 'BuPu_r', 'CMRmap',
-         'CMRmap_r', 'Dark2', 'Dark2_r', 'GnBu', 'GnBu_r', 'Greens', 'Greens_r', 'Greys', 'Greys_r', 'OrRd', 'OrRd_r',
-         'Oranges', 'Oranges_r', 'PRGn', 'PRGn_r', 'Paired', 'Paired_r', 'Pastel1', 'Pastel1_r', 'Pastel2', 'Pastel2_r',
-         'PiYG', 'PiYG_r', 'PuBu', 'PuBuGn', 'PuBuGn_r', 'PuBu_r', 'PuOr', 'PuOr_r', 'PuRd', 'PuRd_r', 'Purples',
-         'Purples_r', 'RdBu', 'RdBu_r', 'RdGy', 'RdGy_r', 'RdPu', 'RdPu_r', 'RdYlBu', 'RdYlBu_r', 'RdYlGn', 'RdYlGn_r',
-         'Reds', 'Reds_r', 'Set1', 'Set1_r', 'Set2', 'Set2_r', 'Set3', 'Set3_r', 'Spectral', 'Spectral_r', 'Vega10',
-         'Vega10_r', 'Vega20', 'Vega20_r', 'Vega20b', 'Vega20b_r', 'Vega20c', 'Vega20c_r', 'Wistia', 'Wistia_r',
-         'YlGn', 'YlGnBu', 'YlGnBu_r', 'YlGn_r', 'YlOrBr', 'YlOrBr_r', 'YlOrRd', 'YlOrRd_r', 'afmhot', 'afmhot_r',
-         'autumn', 'autumn_r', 'binary', 'binary_r', 'bone', 'bone_r', 'brg', 'brg_r', 'bwr', 'bwr_r', 'cool',
-         'cool_r', 'coolwarm', 'coolwarm_r', 'copper', 'copper_r', 'cubehelix', 'cubehelix_r', 'flag', 'flag_r',
-         'gist_earth', 'gist_earth_r', 'gist_gray', 'gist_gray_r', 'gist_heat', 'gist_heat_r', 'gist_ncar',
-         'gist_ncar_r', 'gist_rainbow', 'gist_rainbow_r', 'gist_stern', 'gist_stern_r', 'gist_yarg', 'gist_yarg_r',
-         'gnuplot', 'gnuplot2', 'gnuplot2_r', 'gnuplot_r', 'gray', 'gray_r', 'hot', 'hot_r', 'hsv', 'hsv_r',
-         'inferno', 'inferno_r', 'jet', 'jet_r', 'magma', 'magma_r', 'nipy_spectral', 'nipy_spectral_r', 'ocean',
-         'ocean_r', 'pink', 'pink_r', 'plasma', 'plasma_r', 'prism', 'prism_r', 'rainbow', 'rainbow_r', 'seismic',
-         'seismic_r', 'spectral', 'spectral_r', 'spring', 'spring_r', 'summer', 'summer_r', 'tab10', 'tab10_r',
-         'tab20', 'tab20_r', 'tab20b', 'tab20b_r', 'tab20c', 'tab20c_r', 'terrain', 'terrain_r', 'viridis',
-         'viridis_r', 'winter', 'winter_r']
-
-    @commands.command(brief="Lists colormaps for use with b9!mandelbrot")
-    async def listcolormaps(self,ctx):
-        await ctx.send(str(self.cmaps))
-
-    @commands.command(brief="Plot the mandelbrot set!")
-    @commands.cooldown(1,10,type=commands.BucketType.user)
-    async def mandelbrot(self, ctx, cx : float, cy : float, r : float, p : int, colormap : str):
-        if colormap == "random":
-            cmap = choice(self.cmaps)
-        elif colormap not in self.cmaps:
-            await ctx.send("Uh oh! You friccin moron! `" + colormap + "` isn't a valid colormap! Use `b9!listcolormaps` to find the colormaps!")
-            return
-        else:
-            cmap = colormap
-
-        if p > 5000:
-            return await ctx.send("Uh oh! You friccin moron! That's too many iterations!")
-
-        try:
-            md.render(cx, cy, r, p, cmap)
-        except:
-            await ctx.send("Uh oh it broke. Go yell at baz.")
-            return
-
-        await ctx.send(file=discord.File(open("output.png",mode="rb"),filename="output.png"))
-'''
-
     @commands.command(brief="Factors numbers")
     async def factor(self, ctx, n : int):
 
